fcd8ea42b724d17181f7b6e1bff3a849f9587113
saitokento <246199479+saitokento@users.noreply.github.com>
2026-02-04 00:01:50 +0900

    fix: #20 run.shのポートにデフォルト値を設定

--- streaming_response/run.sh
+++ streaming_response/run.sh
@@ -2,4 +2,4 @@
 
 PATH=$PATH:$LAMBDA_TASK_ROOT/bin \
     PYTHONPATH=$PYTHONPATH:/opt/python:$LAMBDA_RUNTIME_DIR \
-    exec python -m uvicorn --port=$PORT main:app --no-server-header
+    exec python -m uvicorn --port=${PORT:-8000} main:app --no-server-header
