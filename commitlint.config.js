f165cf6243f8c7d18cad65b43850a67167523c45
saitokento <246199479+saitokento@users.noreply.github.com>
2026-04-17 12:53:21 +0900

    chore: #49 commitlint.config.jsから不要なissueNumberキーを削除

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,5 @@
 export default {
   extends: ["@commitlint/config-conventional"],
-  parserPreset: {
-    parserOpts: {
-      issueNumber: ["#"],
-    },
-  },
   rules: {
     "references-empty": [2, "never"],
   },
